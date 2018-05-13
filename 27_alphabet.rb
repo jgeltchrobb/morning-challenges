6cd9ea18941b362ba0f277b8f77340b2f6cc1f48
Jordan Geltch-Robb <jgeltchrobb@hotmail.com>
2018-05-14 09:39:51 +1000

    solved

--- 27_alphabet.rb
+++ 27_alphabet.rb
@@ -1,1 +1,11 @@
 # Have the function AlphabetSoup(str) take the str string parameter being passed and return the string with the letters in alphabetical order (ie. hello becomes ehllo). Assume numbers and punctuation symbols will not be included in the string. 
+
+def alphabet(str)
+  return str.chars.sort_by(&:downcase).join
+end 
+
+puts alphabet("hello")
+puts alphabet("iuasioda")
+puts alphabet("iojas")
+puts alphabet("sdjkhzsb")
+puts alphabet("efcbag")
